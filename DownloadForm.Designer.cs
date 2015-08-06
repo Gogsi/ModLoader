61dadc27b67b47eaddbe8a90a1e1e0703d8bb97f
Gogsi123 <gogogenkov@gmail.com>
2015-08-06 13:18:54 +0300

    Updated datbase

---
+++ DownloadForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace GogsiModLoader
+{
+    partial class DownloadForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.ModName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ModPure = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ModLink = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ModName,
+            this.ModPure,
+            this.ModLink});
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(459, 423);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // ModName
+            // 
+            this.ModName.Text = "Mod Name";
+            this.ModName.Width = 161;
+            // 
+            // ModPure
+            // 
+            this.ModPure.Text = "Is Mod Pure";
+            this.ModPure.Width = 107;
+            // 
+            // ModLink
+            // 
+            this.ModLink.Text = "Mod Download Link";
+            this.ModLink.Width = 205;
+            // 
+            // DownloadForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 447);
+            this.Controls.Add(this.listView1);
+            this.Name = "DownloadForm";
+            this.Text = "DownloadForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader ModName;
+        private System.Windows.Forms.ColumnHeader ModPure;
+        private System.Windows.Forms.ColumnHeader ModLink;
+    }
+}
---
+++ DownloadForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GogsiModLoader
+{
+    public partial class DownloadForm : Form
+    {
+        public DownloadForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                ListViewItem item = listView1.SelectedItems[0];
+                string s_you_want = item.SubItems[1].Text;
+            }
+        }
+    }
+}
